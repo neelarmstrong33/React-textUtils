3a000896582409cf4d76b8100748502dd17b8125
neelarmstrong33 <neelarmstrong33@gmail.com>
2023-06-19 14:29:15 +0530

    Adding Alert Method In The Project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,39 +3,25 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <a className="navbar-brand" href="/">{props.title}</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
         </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">Link</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-            </a>
-          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a className="dropdown-item" href="/">Action</a>
-            <a className="dropdown-item" href="/">Another action</a>
-            <div className="dropdown-divider"></div>
-            <a className="dropdown-item" href="/">Something else here</a>
-          </div>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
         </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+        <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+          <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+        </div>
       </div>
     </nav>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,12 +7,14 @@ export default function TextForm(props) {
         console.log("Uppercase was Clicked" + text);
         let newtext = text.toUpperCase();
         setText(newtext);
+        props.showAlert('Convert to UpperCase', 'success');
     }
 
     const handleLowerClick = () => {
         console.log("Lowercase was Clicked" + text);
         let newtext = text.toLowerCase();
         setText(newtext);
+        props.showAlert('Convert to LowerCase', 'success');
     }
 
     const handleReverseClick = () => {
@@ -41,10 +43,22 @@ export default function TextForm(props) {
         setText(newString); // "olleh"
     }
 
+    const handleCopy = () => {
+        var text = document.getElementById('mybox');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
     const handleOnChange = (event) => {
         console.log("On Change");
         setText(event.target.value);
     }
+
+    const handleClear = () => {
+        let newText = '';
+        setText(newText);
+    }
+    
 
     const[text, setText] = useState("");
 
@@ -53,21 +67,24 @@ export default function TextForm(props) {
 
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color: props.mode === 'dark' ?'white':'black'}}>
         <h1>{props.heading}</h1>
         <div className='mb-3'>
-            <textarea className='form-control' value={text} onChange={handleOnChange} id='mybox' rows={6}></textarea>
+            <textarea className='form-control' value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black',}}
+             id='mybox' rows={6}></textarea>
         </div>
         <button className='btn btn-primary mx-1' onClick={handleUpClick}>Convert to UpperCase</button>
         <button className='btn btn-primary mx-1' onClick={handleLowerClick}>Convert to LowerCase</button>
         <button className='btn btn-primary mx-1' onClick={handleReverseClick}>Reverse the text</button>
+        <button className='btn btn-primary mx-1' onClick={handleCopy}>Copy Text</button>
+        <button className='btn btn-primary mx-1' onClick={handleClear}>Clear Text</button>
     </div>
-    <div className='container'>
+    <div className='container' style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
         <h2>Your text Summary</h2>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length } minutes to read</p>
         <h2>PREVIEW</h2>
-        <p>{text}</p>
+        <p>{text.length>0 ? text : `Enter Something in the textBox Above to Preview it here`}</p>
     </div>
     </>    
   );
