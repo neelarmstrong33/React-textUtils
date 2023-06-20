27d532247b3f055953e66f31704505e277681350
Neel Jain <128790940+neelarmstrong33@users.noreply.github.com>
2023-06-20 10:13:02 +0530

    Adding Router features In the Project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Alert from './Alert';
+import Alert from './components/Alert';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 
 function App() {
@@ -28,22 +32,34 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = 'grey';
       showAlert('Dark mode has been enabled', 'success');
+      // document.title = 'TextUtils -Dark mode';
+      // setInterval(() => {
+      //   document.title = 'TextUtils is Amazing';
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = 'Install TextUtils Now';
+      // }, 1500);
     } 
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert('Light mode has been enabled', 'success');
+      document.title = 'TextUtils -Dark mode';
     }
 
   }
   return (
     <>
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-    <Alert alert={alert} />
-    <div className="container my-3">
-    <TextForm showAlert={showAlert} heading="Enter the text to Analyze" mode={mode} />
-    {/* <About/> */}
-    </div>
+    <Router>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About/>} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to Analyze" mode={mode} />} />
+          </Routes>
+        </div>
+    </Router>
     </>
   );
 }
